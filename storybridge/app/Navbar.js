a23c53217607edda947cd141d38ea8cc8ab3b141
isabellaqian <isabellajqian@gmail.com>
2024-02-03 14:40:02 -0800

    nav bar complete

--- storybridge/app/Navbar.js
+++ storybridge/app/Navbar.js
@@ -3,11 +3,16 @@ import styles from "./Navbar.module.css";
 export default function Navbar() {
   return (
     <div className={styles.main}>
-      <img src="/logo.svg" alt="logo" width="100" />
-      <h1>hello</h1>
-      <div>Home</div>
-      <div>Schedule Session</div>
-      <div>Shop</div>
+      <div className={styles.left}>
+        <img src="/logo.svg" alt="logo" width="100" />
+        <h1 style={{ "font-weight": "normal" }}>Home</h1>
+        <h1 style={{ "font-weight": "normal" }}>Schedule Session</h1>
+        <h1 style={{ "font-weight": "normal" }}>Shop</h1>
+      </div>
+      <div className={styles.right}>
+        <img src="/coin.svg" alt="coin" width="50" />
+        <img src="/profile.svg" alt="profile" width="50" />
+      </div>
     </div>
   );
 }
