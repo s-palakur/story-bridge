23778644921698917806295210c92667cf0648fd
isabellaqian <isabellajqian@gmail.com>
2024-02-03 19:45:39 -0800

    shop page skeleton

---
+++ storybridge/app/shop/page.jsx
@@ -0,0 +1,16 @@
+"use client";
+import React, { useState } from "react";
+import styles from "./page.module.css";
+import Navbar from "../Navbar";
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+      <h1>
+        Answer questions during storytime to earn gold. Spend your gold here to
+        customize your avatar!
+      </h1>
+    </div>
+  );
+}
