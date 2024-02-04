ce1160760c0c84a985c02436724fb00f173366a5
Amy Li <amy.al12345@gmail.com>
2024-02-04 05:51:29 -0800

    fixed countdown timer

--- storybridge/app/dashboard/CountdownTimer.js
+++ storybridge/app/dashboard/CountdownTimer.js
@@ -4,15 +4,15 @@ import styles from "./CountdownTimer.module.css";
 
 
 
-const CountdownTimer = ({ targetDate }) => {
+const CountdownTimer = () => {
+  const targetDate = new Date("February 6, 2024 16:00:00").getTime(); // Target date and time: 4 PM, February 6, 2024
+
   const calculateTimeRemaining = () => {
     const now = new Date().getTime();
     const difference = targetDate - now;
 
     const days = Math.floor(difference / (1000 * 60 * 60 * 24));
-    const hours = Math.floor(
-      (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-    );
+    const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((difference % (1000 * 60)) / 1000);
 
@@ -27,7 +27,7 @@ const CountdownTimer = ({ targetDate }) => {
     }, 1000);
 
     return () => clearInterval(interval);
-  }, [targetDate]);
+  }, []);
 
 
   const openGoogleMeet = () => {
